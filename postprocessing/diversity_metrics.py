be210009e9f9f211ada668c9e4c00ec29772c86a
An Pham <An.Pham@nlr.gov>
2026-06-29 12:47:32 -0600

    reformat & clean up

--- postprocessing/diversity_metrics.py
+++ postprocessing/diversity_metrics.py
@@ -41,10 +41,10 @@ def main():
     submetrics = ['pv', 'wind-ons', 'wind-ofs', 'coal', 'gas', 'nuclear', 'all' ]           
     year = 2050
     number_of_max_diff_case = 10
-    runs_path = '/Users/apham/Documents/GitHub/ReEDS/public_ReEDS/ReEDS/runs/rvs'     # Path of runs folder
-    case_file = '/Users/apham/Documents/GitHub/ReEDS/public_ReEDS/ReEDS/rv_runs_test.csv'  # Case file in csv that includes all case names to compare
-    #runs_path = '/kfs2/projects/uncertainty/apham/ReEDS/runs'     # Path of runs folder
-    #case_file = '/kfs2/projects/uncertainty/apham/ReEDS/rv_runs.csv'  # Case file in csv that includes all case names to compare
+    #runs_path = '/Users/apham/Documents/GitHub/ReEDS/public_ReEDS/ReEDS/runs/rvs'     # Path of runs folder
+    #case_file = '/Users/apham/Documents/GitHub/ReEDS/public_ReEDS/ReEDS/rv_runs_test.csv'  # Case file in csv that includes all case names to compare
+    runs_path = '/kfs2/projects/uncertainty/apham/ReEDS/runs'     # Path of runs folder
+    case_file = '/kfs2/projects/uncertainty/apham/ReEDS/rv_runs.csv'  # Case file in csv that includes all case names to compare
     #########################################################################################################
     
     # Read in data case file:
