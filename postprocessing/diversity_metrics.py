11cf70c8cc03aac4fa687c9063e6811189b416b4
An Pham <An.Pham@nlr.gov>
2026-06-29 12:44:15 -0600

    clean up gini index calculation

--- postprocessing/diversity_metrics.py
+++ postprocessing/diversity_metrics.py
@@ -1,5 +1,6 @@
 #%% Imports
 import pandas as pd
+import numpy as np
 import matplotlib as mpl
 import matplotlib.pyplot as plt
 import os
@@ -40,10 +41,10 @@ def main():
     submetrics = ['pv', 'wind-ons', 'wind-ofs', 'coal', 'gas', 'nuclear', 'all' ]           
     year = 2050
     number_of_max_diff_case = 10
-    #runs_path = '/Users/apham/Documents/GitHub/ReEDS/public_ReEDS/ReEDS/runs/rvs'     # Path of runs folder
-    #case_file = '/Users/apham/Documents/GitHub/ReEDS/public_ReEDS/ReEDS/rv_runs_test.csv'  # Case file in csv that includes all case names to compare
-    runs_path = '/kfs2/projects/uncertainty/apham/ReEDS/runs'     # Path of runs folder
-    case_file = '/kfs2/projects/uncertainty/apham/ReEDS/rv_runs.csv'  # Case file in csv that includes all case names to compare
+    runs_path = '/Users/apham/Documents/GitHub/ReEDS/public_ReEDS/ReEDS/runs/rvs'     # Path of runs folder
+    case_file = '/Users/apham/Documents/GitHub/ReEDS/public_ReEDS/ReEDS/rv_runs_test.csv'  # Case file in csv that includes all case names to compare
+    #runs_path = '/kfs2/projects/uncertainty/apham/ReEDS/runs'     # Path of runs folder
+    #case_file = '/kfs2/projects/uncertainty/apham/ReEDS/rv_runs.csv'  # Case file in csv that includes all case names to compare
     #########################################################################################################
     
     # Read in data case file:
@@ -64,9 +65,15 @@ def main():
     case_file['rank_coal'] = 0
     case_file['rank_gas'] = 0
     case_file['rank_nuclear'] = 0
+    case_file['gini_pv'] = 0
+    case_file['gini_wind-ons'] = 0
+    case_file['gini_wind-ofs'] = 0
+    case_file['gini_coal'] = 0
+    case_file['gini_gas'] = 0
+    case_file['gini_nuclear'] = 0
     
-    # Find the maximally different solution from optimal solution:
     for submetric in submetrics:
+        # Find the maximally different solution from optimal solution:
         case_file = euclidean_distance_calc(runs_path, case_file, optimal_case, rv_cases, metric, submetric, year)
         if submetric =='all':
             col = 'euclidean_distance'
@@ -75,6 +82,14 @@ def main():
             col = 'euclidean_distance_'+submetric
             rank = 'rank_'+submetric
         case_file.loc[case_file['old_prefix']==case_file.loc[case_file[col].idxmax(), 'old_prefix'], rank] = 1
+        
+        # Calculate Gini index
+        if submetric == 'all':
+            continue
+        else:
+            case_file = gini_coefficient_cal(case_file, runs_path, metric, submetric, year)
+            
+                
     
 
     # Find the next number_of_max_diff_case maximallty different solutions from 
@@ -83,7 +98,7 @@ def main():
     #    rv_cases_i = rv_cases.remove(case_file.loc[case_file['rank']>i, 'old_prefix'])
     #    sel_cases_i = optimal_case.append(case_file.loc[case_file['rank']>i, 'old_prefix'])
 
-    # Calculate Gini index
+    
 
     case_file.to_csv(os.path.join(runs_path,'case_diversity_metrics.csv'))    
 
@@ -118,4 +133,27 @@ def euclidean_distance_calc(runs_path, case_file, optimal_case, rv_cases, metric
         case_file.loc[case_file['old_prefix']==case,col] = euclidean_dist
     return case_file
 
+def gini_coefficient_cal(case_file, runs_path, metric, submetric, year):
+    for case in case_file['old_prefix'].unique().tolist():
+        if metric == 'capacity':
+            file = 'cap.csv'
+        elif metric == 'generation':
+            file = 'gen_ann.csv'
+
+        output_path_gi = os.path.join(runs_path,case,'outputs')    
+        data = pd.read_csv(os.path.join(output_path_gi,file))
+        data = data[data['t']==year]
+        data = data[data['i'].str.contains(submetric)]
+        data = data.drop(columns=['i','t'])
+        data = data.groupby(['r'], as_index=False).agg({'Value': 'sum'})
+
+        total = 0
+        for i, xi in enumerate(data['Value'][:-1],1):
+            total += np.sum(np.abs(xi-data['Value'][i:]))
+            gini = total / (len(data['Value'])**2 * np.mean(data['Value']))
+        col = 'gini_'+submetric
+        case_file.loc[case_file['old_prefix']==case,col] = gini  
+
+    return case_file  
+    
 main()
