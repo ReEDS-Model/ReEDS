4995212008a078e789b330b3b3ff06bd139cd047
An Pham <An.Pham@nlr.gov>
2026-07-20 07:09:29 -0600

    Update diversity_metrics.py

--- postprocessing/diversity_metrics.py
+++ postprocessing/diversity_metrics.py
@@ -37,33 +37,45 @@ def main():
     #case_file = args.case_file                      # Case file in csv that includes all case names to compare.
     
     ######################################### FOR TESTING/DEBUGGING #########################################
-    run_on = 'local'                                                                # kestrel vs local  
-    metric = 'capacity'                                                               # Metric to calculate distance: 'capacity', 'generation'
-    #submetrics = ['pv','wind-ons','wind-ofs','gas','coal','gentech']      
+    run_on = 'kestrel'                                                    # kestrel vs local  
+    metric = 'cap'                                                      # Metric to calculate distance:
+                                                                        # 'cap', 'gen', 'weighted_gen', 'emission', 
+                                                                        # 'weighted_emission', 'employment', 
+                                                                        # 'weighted_employment'
+    #submetrics = ['pv','wind-ons','wind-ofs','gas','coal','gentech']   # only if metric = cap or gen  
     submetrics = ['gentech']     
     year = 2050
-    number_of_max_diff_case = 100
+    ed_or_hmsed = 'ED'                                                  # 'ED' or 'HMSED'
+    number_of_max_diff_case = 100                                       # Only if ed_or_hmsed = 'HMSED'
 
     if run_on == 'local':
         runs_path = '/Users/apham/Documents/Projects/ReEDS_Projects/FY26/Uncertainty/MGA_Paper_2/runs'          # Path of runs folder
         case_file_orig = '/Users/apham/Documents/GitHub/ReEDS/public_ReEDS/ReEDS/rv_runs_test.csv'  # Case file in csv that includes all case names to compare
     elif run_on == 'kestrel':
         runs_path = '/kfs2/projects/uncertainty/apham/ReEDS/runs'                       # Path of runs folder
-        case_file_orig = '/kfs2/projects/uncertainty/apham/ReEDS/rv_runs_gentech_completed.csv'  # Case file in csv that includes all case names to compare
+        case_file_orig = '/kfs2/projects/uncertainty/apham/ReEDS/rv_runs_all_completed.csv'  # Case file in csv that includes all case names to compare
     #########################################################################################################
     
     # Read in data case file:
-    case_file_orig = pd.read_csv(case_file_orig)
-    optimal_case = case_file_orig['scenario'].iloc[0]
-    rv_cases = case_file_orig['scenario'].iloc[1:].values.tolist()
-    min_max_cases = case_file_orig[(case_file_orig['scenario'].str.contains('min')) | 
-                          (case_file_orig['scenario'].str.contains('max'))]['scenario'].tolist()
+    case_file = pd.read_csv(case_file_orig)
+    optimal_case = case_file['scenario'].iloc[0]
+    rv_cases = case_file['scenario'].iloc[1:].values.tolist()
+    min_max_cases = case_file[(case_file['scenario'].str.contains('min')) | 
+                          (case_file['scenario'].str.contains('max'))]['scenario'].tolist()
     rv_cases = [x for x in rv_cases if x not in min_max_cases]
 
-    if metric == 'capacity':
+    if metric == 'cap':
         file = 'cap.csv'
-    elif metric == 'generation':
+    elif (metric == 'gen') | (metric == 'weighted_gen'):
         file = 'gen_ann.csv'
+    elif (metric == 'emission') | (metric == 'weighted_emission'):
+        file = 'emit_r.csv'
+    elif (metric == 'employment') | (metric == 'weighted_employment'):
+        file = 'employment_tot.csv'
+
+    data = data_clean(runs_path,case_file,file,year,metric)
+    data.to_csv(os.path.join(runs_path,metric+'.csv'),index=False)
+
    
     for submetric in submetrics:
         if submetric != 'gentech':
@@ -80,13 +92,13 @@ def main():
             rank_HMSED = 'HMSED_rank'
             col_gini = 'gini' 
         
-        rv_cases = [item for item in rv_cases if submetric in item]
-        case_file = case_file_orig[case_file_orig['scenario'].str.contains(submetric)]
-    
+        #rv_cases = [item for item in rv_cases if submetric in item]
+        #case_file = case_file[case_file['scenario'].str.contains(submetric)]
+    '''
         # Find maximally different solutions
         case_file = euclidean_distance_calc(runs_path, case_file, optimal_case, 
                                             rv_cases, submetric, file, year,number_of_max_diff_case,
-                                            col_ED, rank_ED, col_HMSED, rank_HMSED)
+                                            col_ED, rank_ED, col_HMSED, rank_HMSED, ed_or_hmsed)
         
         # Identify top most maximally different solutions
         max_diff_solutions = case_file.sort_values(by=rank_HMSED)
@@ -127,12 +139,67 @@ def main():
                         max_diff_solutions, spatially_diff_solutions)
         
 
-
+'''
 ######################################################################################################
 #%% FUNCTIONS ###
+def data_clean(runs_path,case_file,file,year,metric):
+    population_fips = pd.read_csv(os.path.join(reeds_path,'inputs','disaggregation','county_population.csv'))
+    county2zone = reeds.io.get_county2zone(GSw_ZoneSet='z54', as_map=False)
+    county2zone['FIPS'] = 'p' + county2zone.FIPS
+    county2zone = county2zone[['FIPS','r']]
+    population_fips = population_fips.merge(county2zone, on='FIPS',how='left')
+    population_r = population_fips.groupby(['r'], as_index=False).agg({'value': 'sum'}).rename(columns={'value':'population'})
+
+    # Clean up and format raw data:
+    data_scenario_base = pd.DataFrame()
+    for scenario in case_file['scenario'].unique().tolist():
+        data_scenario = pd.read_csv(os.path.join(runs_path,scenario,'outputs',file))
+        data_scenario = data_scenario[data_scenario['t']==year].drop(columns='t')
+        if 'emission' in metric:
+            data_scenario = data_scenario[(data_scenario['eall']=='CO2') & (data_scenario['etype']=='process')]
+            data_scenario = data_scenario[['r','Value']]
+        elif 'employment' in metric:
+            data_scenario = data_scenario[['r','Value']]
+        else:
+            data_scenario['i'] = data_scenario['i'].str.lower()
+            tech_map = pd.read_csv(os.path.join(reeds_path,'postprocessing','tech_aggregation.csv')
+                                ).rename(columns={'raw':'i','display_aggregated':'tech'}
+                                            ).drop(columns=['display','diagnostics','ther_stor_vre'])
+            tech_map.loc[tech_map['tech']=='Solar','tech'] = 'Solar PV'
+
+            data_scenario = data_scenario.merge(tech_map,on='i',how='left')
+
+            data_scenario.loc[data_scenario['i'].str.contains('upv'),'tech'] = 'Solar PV'
+            data_scenario.loc[data_scenario['i'].str.contains('wind-ons'),'tech'] = 'Land-based Wind'
+            data_scenario.loc[data_scenario['i'].str.contains('wind-ofs'),'tech'] = 'Offshore Wind'
+            data_scenario.loc[data_scenario['i'].str.contains('geohydro_allkm'),'tech'] = 'Geothermal'
+            data_scenario.loc[data_scenario['i'].str.contains('egs'),'tech'] = 'Geothermal'
+            data_scenario.loc[data_scenario['i'].isin(['hydnd','hydud','hydund','hydnpnd','hyded','hydend']),'tech'] = 'Hydro'
+
+            data_scenario = data_scenario.groupby(['tech','r'], as_index=False).agg({'Value': 'sum'})
+            data_scenario = data_scenario[data_scenario['tech']!='Remove']
+        
+        if 'weighted' in metric:
+            if 'emission' in metric:
+                data_scenario.loc[data_scenario['Value']<0,'Value'] = 0
+            data_scenario = data_scenario.merge(population_r,on='r',how='left')
+            data_scenario['Value'] = data_scenario['Value']/data_scenario['population']
+            data_scenario = data_scenario.drop(columns='population')
+        data_scenario = data_scenario.rename(columns={'Value':scenario})
+        if 'Optimal' in scenario:
+            data_scenario_base = data_scenario.copy()
+        else:
+            if ('emission' in metric ) | ('employment' in metric ):
+                data_scenario_base = data_scenario_base.merge(data_scenario,on=['r'],how='outer')
+            else:
+                data_scenario_base = data_scenario_base.merge(data_scenario,on=['tech','r'],how='outer')
+            data_scenario_base = data_scenario_base.fillna(0)
+
+    return data_scenario_base
+        
 def euclidean_distance_calc(runs_path, case_file, optimal_case,
                             rv_cases, submetric, file, year, number_of_max_diff_case,
-                            col_ED, rank_ED, col_HMSED, rank_HMSED):
+                            col_ED, rank_ED, col_HMSED, rank_HMSED, ed_or_hmsed):
 
     output_path_optimal = os.path.join(runs_path,optimal_case,'outputs')
     data_optimal = pd.read_csv(os.path.join(output_path_optimal,file)).rename(columns={'Value':optimal_case})
@@ -179,29 +246,30 @@ def euclidean_distance_calc(runs_path, case_file, optimal_case,
     case_file.loc[case_file[rank_ED]==1,col_HMSED] = case_file.loc[case_file[rank_ED]==1,col_ED].iloc[0]
     case_file.loc[case_file[rank_ED]==1,rank_HMSED] = case_file.loc[case_file[rank_ED]==1,rank_ED].iloc[0]
 
-    # calculate harmonic mean squared of euclidean distance (HMSED) 
-    # to find the next number_of_max_diff_case maximally different solutions
-    # HMSED formula from https://doi.org/10.1016/j.energy.2017.03.043 
-    #for case_max_diff in list(range(len(rv_cases)-1)):
-    for case_max_diff in list(range(number_of_max_diff_case)):
-        print(f"{case_max_diff} maximally different solution(s) in terms of {submetric}")
-        
-        # set of selected cases (cost+optimal + previous maximally different scenarios)
-        rv_cases_i = case_file.loc[case_file[rank_ED]>case_max_diff+1, 'scenario'].tolist()
-        # set of random vector cases to calculate HMSED
-        sel_cases_i = [x for x in case_file['scenario'].tolist() if x not in rv_cases_i]
+    if ed_or_hmsed == 'HMSED':
+        # calculate harmonic mean squared of euclidean distance (HMSED) 
+        # to find the next number_of_max_diff_case maximally different solutions
+        # HMSED formula from https://doi.org/10.1016/j.energy.2017.03.043 
+        #for case_max_diff in list(range(len(rv_cases)-1)):
+        for case_max_diff in list(range(number_of_max_diff_case)):
+            print(f"{case_max_diff} maximally different solution(s) in terms of {submetric}")
+            
+            # set of selected cases (cost+optimal + previous maximally different scenarios)
+            rv_cases_i = case_file.loc[case_file[rank_ED]>case_max_diff+1, 'scenario'].tolist()
+            # set of random vector cases to calculate HMSED
+            sel_cases_i = [x for x in case_file['scenario'].tolist() if x not in rv_cases_i]
 
-        # calculate HMSED from each case in set of rv cases to set of selected cases:
-        inverse_sum_diff = 0
-        for i in rv_cases_i:
-            for j in sel_cases_i:
-                inverse_sum_diff += 1/sum((data_rv_sub[i]-data_rv_sub[j])**2)
-            inverse_sum_diff_i = inverse_sum_diff**(-1)
-            case_file.loc[case_file['scenario']==i,col_HMSED] = inverse_sum_diff_i 
-        max_diff_case = case_file.loc[case_file['scenario'].isin(rv_cases_i)]
-        max_diff_case = max_diff_case.loc[max_diff_case[col_HMSED].idxmax(), 'scenario']
-        case_file.loc[case_file['scenario']==max_diff_case, rank_HMSED] = case_max_diff + 2
-        case_file.loc[case_file['scenario']==max_diff_case, rank_ED] = case_file[rank_HMSED]
+            # calculate HMSED from each case in set of rv cases to set of selected cases:
+            inverse_sum_diff = 0
+            for i in rv_cases_i:
+                for j in sel_cases_i:
+                    inverse_sum_diff += 1/sum((data_rv_sub[i]-data_rv_sub[j])**2)
+                inverse_sum_diff_i = inverse_sum_diff**(-1)
+                case_file.loc[case_file['scenario']==i,col_HMSED] = inverse_sum_diff_i 
+            max_diff_case = case_file.loc[case_file['scenario'].isin(rv_cases_i)]
+            max_diff_case = max_diff_case.loc[max_diff_case[col_HMSED].idxmax(), 'scenario']
+            case_file.loc[case_file['scenario']==max_diff_case, rank_HMSED] = case_max_diff + 2
+            case_file.loc[case_file['scenario']==max_diff_case, rank_ED] = case_file[rank_HMSED]
 
     return case_file
 
