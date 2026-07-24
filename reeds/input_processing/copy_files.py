63e48778ba8a7356d2bd176d64eefe074ce20a54
An Pham <An.Pham@nlr.gov>
2026-07-24 14:18:39 -0600

    Update copy_files.py

--- reeds/input_processing/copy_files.py
+++ reeds/input_processing/copy_files.py
@@ -997,6 +997,16 @@ def write_miscellaneous_files(
         index_col='i',
     )[sw['GSw_H2LeakageScen']].rename_axis('*i').round(5).to_csv(
         os.path.join(inputs_case,'h2_leakage_rate.csv'))
+
+    # Add coal emission rate multiplier by FIPS
+    emitrate_coal_mult = pd.read_csv(os.path.join(inputs_case,'emitrate_coal_multiplier.csv'))
+    # Aggregate based on spatial resolution
+    county2zone = reeds.io.get_county2zone(case=os.path.dirname(inputs_case))
+    county2zone.index = 'p' + county2zone.index
+    emitrate_coal_mult['r'] = emitrate_coal_mult['FIPS'].map(county2zone)
+    emitrate_coal_mult = emitrate_coal_mult.drop(columns=['FIPS'])
+    emitrate_coal_mult = emitrate_coal_mult.groupby(['i','r','etype'], as_index=False).mean()
+    emitrate_coal_mult.to_csv(os.path.join(inputs_case,'emitrate_coal_mult.csv'), index=False)
     
     # Transmission employment factors:
     ef_trans = pd.read_csv(
@@ -1004,18 +1014,6 @@ def write_miscellaneous_files(
         index_col=0)
     ef_trans[ef_trans.index == sw['GSw_EmploymentFactor']].T.round(8).to_csv(
         os.path.join(inputs_case,'employment_factor_inter_transmission.csv'),header=False)
-    
-    # Add coal emission rate multiplier by FIPS
-    emitrate_coal_mult = pd.read_csv(os.path.join(inputs_case,'emitrate_coal_multiplier.csv'))
-    # Aggregate based on spatial resolution
-    county2zone = reeds.io.get_county2zone(case=os.path.dirname(inputs_case))
-    county2zone.index = 'p' + county2zone.index
-    emitrate_coal_mult['r'] = emitrate_coal_mult['FIPS'].map(county2zone)
-    
-    emitrate_coal_mult = emitrate_coal_mult.drop(columns=['FIPS'])
-    emitrate_coal_mult = emitrate_coal_mult.groupby(['i','r','etype'], as_index=False).mean()
-    emitrate_coal_mult.to_csv(os.path.join(inputs_case,'emitrate_coal_mult.csv'), index=False)
-
 
     # Add this_year to years_until_endogenous to generate the tech-specific firstyear.csv file
     scalars = reeds.io.get_scalars(full=True)
