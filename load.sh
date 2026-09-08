e94c896d7371249553fa2850a4903e1b450ea729
cavraam <Charalampos.Avraam@nrel.gov>
2026-09-08 15:51:01 -0600

    create supporting scripts

---
+++ load.sh
@@ -0,0 +1,8 @@
+module purge
+module load git
+module load git-lfs
+module load anaconda3
+module load gams
+
+conda deactivate
+conda activate reeds
---
+++ srun_debug.sh
@@ -0,0 +1,2 @@
+srun --time=1:00:00  --account=finitoreeds --nodes=1  --partition=debug --pty $SHELL
+# salloc --time=240 --account=finitoreeds --nodes=1 --partition=debug
