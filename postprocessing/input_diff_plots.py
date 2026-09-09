e1cc112612bf6800b6ca7b2fd660eb8d7b7a9aa3
Patrick Brown <25125211+patrickbrown4@users.noreply.github.com>
2026-09-09 09:07:58 -0600

    add input_diff_plots.plot_sc_diffs()

--- postprocessing/input_diff_plots.py
+++ postprocessing/input_diff_plots.py
@@ -4,6 +4,8 @@ import sys
 import argparse
 import pandas as pd
 import geopandas as gpd
+from pathlib import Path
+from typing import Literal
 import matplotlib.pyplot as plt
 from matplotlib import patheffects as pe
 import cmocean
@@ -21,10 +23,14 @@ def plot_diff_maps(
     dfmap,
     data,
     title='',
-    value_name='CF',
     cmap=plt.cm.turbo,
     cmap_diff=plt.cm.RdBu_r,
-    diffmax=2,
+    diffmax=None,
+    scale=1,
+    value_name='CF',
+    units='%',
+    vmin=None,
+    vmax=None,
 ):
     """
     Plot absolute and difference maps.
@@ -44,22 +50,22 @@ def plot_diff_maps(
     """
     nrows = 1
     ncols = 3
-    scale = 4
-    vmin = 0.
-    vmax = max([data[case].max() for case in data]) * 100
+    figscale = 4
+    vmin = (0. if vmin is None else vmin) * scale
+    vmax = (max([data[case].max() for case in data]) if vmax is None else vmax) * scale
 
     dfr = (dfmap['r'] if isinstance(dfmap, dict) else dfmap)
 
     plt.close()
     f,ax = plt.subplots(
-        nrows, ncols, sharex=True, sharey=True, figsize=(scale*ncols, scale*nrows*0.75),
+        nrows, ncols, sharex=True, sharey=True, figsize=(figscale*ncols, figscale*nrows*0.75),
         gridspec_kw={'wspace':0},
     )
     ## Absolute
     for (col, case) in enumerate(data.keys()):
         _ax = ax[col]
         df = dfr.copy()
-        df['value'] = data[case] * 100
+        df['value'] = data[case] * scale
         if 'st' in dfmap:
             dfmap['st'].plot(ax=_ax, facecolor='none', edgecolor='w', lw=0.1, zorder=1e7)
         if 'country' in dfmap:
@@ -67,7 +73,7 @@ def plot_diff_maps(
         df.plot(ax=_ax, column='value', vmin=vmin, vmax=vmax, cmap=cmap)
         reeds.plots.addcolorbarhist(
             f, _ax, df['value'].values, vmin=vmin, vmax=vmax, cmap=cmap,
-            title=f"{value_name} ({case}) [%]",
+            title=f"{value_name} ({case}) [{units}]",
             nbins=51, cbarheight=0.8, cbarwidth=0.04, histratio=2,
             orientation='horizontal', cbarbottom=-0.1, labelpad=3.3,
         )
@@ -76,8 +82,8 @@ def plot_diff_maps(
     col = 2
     _ax = ax[col]
     df = dfr.copy()
-    df['value'] = (data['new'] - data['old']) * 100
-    _diffmax = (df['value'].abs().max() if not diffmax else diffmax)
+    df['value'] = (data['new'] - data['old']) * scale
+    _diffmax = max((df['value'].abs().max() if not diffmax else diffmax), 0.1)
     if 'st' in dfmap:
         dfmap['st'].plot(ax=_ax, facecolor='none', edgecolor='0.9', lw=0.1, zorder=1e7)
     if 'country' in dfmap:
@@ -85,7 +91,7 @@ def plot_diff_maps(
     df.plot(ax=_ax, column='value', vmin=-_diffmax, vmax=_diffmax, cmap=cmap_diff)
     reeds.plots.addcolorbarhist(
         f, _ax, df['value'].values, vmin=-_diffmax, vmax=_diffmax, cmap=cmap_diff,
-        title=f"{value_name} diff (new - old) [%]",
+        title=f"{value_name} diff (new - old) [{units}]",
         nbins=51, cbarheight=0.8, cbarwidth=0.04, histratio=2,
         orientation='horizontal', cbarbottom=-0.1, labelpad=3.3,
     )
@@ -177,9 +183,78 @@ def plot_cf_diff(
         cmap=cmap,
         cmap_diff=cmap_diff,
         diffmax=diffmax,
+        scale=100,
     )
 
     return f, ax
+
+
+def get_supplycurves(
+    repo_old,
+    repo_new,
+    tech:Literal['upv','wind-ons','egs',None]='wind-ons',
+    access:Literal['limited','reference','open',None]='reference',
+    crs:str='EPSG:5070',
+) -> dict:
+    """Get supply curves from two repos"""
+    dfs = {}
+    for label, repo in [('old', repo_old), ('new', repo_new)]:
+        sitespath = Path(repo, 'inputs', 'supply_curve', 'interconnection_land.h5')
+        dfsites = reeds.plots.df2gdf(
+            reeds.io.floatify(reeds.io.read_h5_groups(sitespath)),
+            crs=crs,
+        )
+        dfsites = reeds.spatial.site2poly_buffer(dfsites)
+        if (tech is not None) and (access is not None):
+            scpath = Path(repo, 'inputs', 'supply_curve', f'supplycurve_{tech}-{access}.csv')
+            dfsc = pd.read_csv(scpath, index_col='sc_point_gid')
+            dfs[label] = dfsites.merge(dfsc, left_index=True, right_index=True, how='right')
+        elif (tech is None) and (access is None):
+            dfs[label] = dfsites
+        else:
+            raise ValueError(f'If providing tech ({tech}) must provide access ({access})')
+    return dfs
+
+
+def plot_sc_diffs(
+    repo_old,
+    repo_new,
+    tech:Literal['upv','wind-ons','egs',None]='wind-ons',
+    access:Literal['limited','reference','open',None]='reference',
+    crs:str='EPSG:5070',
+    cmap=cmocean.cm.rain,
+    cmap_diff=plt.cm.RdBu_r,
+    interconnection=False,
+):
+    """Plot difference maps of supply curve columns"""
+    data = get_supplycurves(repo_old, repo_new, tech, access, crs)
+    if tech and access:
+        column_units = {
+            'capacity': 'MW',
+            'capital_adder_per_mw': '$/MW',
+            'cf': '.',
+            'class': 'n/a',
+        }
+    else:
+        column_units = {}
+    if interconnection:
+        column_units.update({
+            'dist_spur_km': 'km',
+            'dist_reinforcement_km': 'km',
+            'cost_spur_usd_per_mw': '$/MW',
+            'cost_reinforcement_usd_per_mw': '$/MW',
+            'cost_total_trans_usd_per_mw': '$/MW',
+    })
+    for column, units in column_units.items():
+        dfs = {key: df[column] for key, df in data.items()}
+        f, ax = plot_diff_maps(
+            dfmap=data['old'][['geometry']],
+            data=dfs,
+            title=f"{tech} {access}",
+            cmap=cmap, cmap_diff=cmap_diff,
+            value_name=column, units=units,
+        )
+        yield f, ax, column
 
 
 def plot_distpv_diff(
@@ -212,6 +287,7 @@ def plot_distpv_diff(
         cmap=cmap,
         cmap_diff=cmap_diff,
         diffmax=diffmax,
+        scale=100,
     )
 
     return f, ax
@@ -224,7 +300,7 @@ def plot_prm_diff(
     year=None,
     cmap=cmocean.cm.tempo,
     cmap_diff=plt.cm.RdBu_r,
-    diffmax=False,
+    diffmax=None,
 ):
     repos = {'old': repo_old, 'new': repo_new}
     dfprm = {}
@@ -268,6 +344,7 @@ def plot_prm_diff(
         cmap=cmap,
         cmap_diff=cmap_diff,
         diffmax=diffmax,
+        scale=100,
     )
 
     return f, ax
@@ -280,7 +357,7 @@ def main(repo_old, repo_new, outpath):
             raise FileNotFoundError(repo)
     os.makedirs(outpath, exist_ok=True)
 
-    ## CF difference
+    ## Capacity factors
     for tech, special in [
         ('wind-ofs', 'meshed'),
         ('wind-ofs', 'radial'),
@@ -299,8 +376,35 @@ def main(repo_old, repo_new, outpath):
     plot_distpv_diff(repo_old, repo_new, diffmax=diffmax)
     plt.savefig(os.path.join(outpath, 'cf_diff-distpv.png'))
 
+    ## Planning reserve margin
     plot_prm_diff(repo_old, repo_new, prm_column='nerc')
     plt.savefig(os.path.join(outpath, 'prm_diff-nerc.png'))
+
+    ## Supply curves
+    tech_accesses = [
+        ('upv', 'limited'),
+        ('upv', 'reference'),
+        ('upv', 'open'),
+        ('wind-ons', 'limited'),
+        ('wind-ons', 'reference'),
+        ('wind-ons', 'open'),
+        ('egs', 'reference'),
+        (None, None),
+    ]
+    for tech, access in tech_accesses:
+        label = 'interconnection' if tech is None else f"{tech.replace('-','')}-{access}"
+        plot_generator = plot_sc_diffs(
+            repo_old, repo_new, tech=tech, access=access,
+            interconnection=(True if tech is None else False),
+        )
+        while True:
+            try:
+                f, ax, column = next(plot_generator)
+                fpath = Path(outpath, f'sc_diff-{label}-{column}.png')
+                plt.savefig(fpath)
+                print(fpath)
+            except StopIteration:
+                break
 
 
 #%% Procedure
@@ -320,6 +424,14 @@ if __name__ == '__main__':
     repo_old = os.path.expanduser(args.old)
     repo_new = os.path.expanduser(args.new)
     outpath = os.path.expanduser(args.outpath)
+
+    # #%% Inputs for testing
+    # repo_old = Path('~/github2/ReEDS').expanduser()
+    # repo_new = Path('~/github3/ReEDS').expanduser()
+    # from datetime import datetime
+    # today = datetime.now().strftime('%Y%m%d')
+    # outpath = Path(f'~/scratch/{today}').expanduser()
+    # outpath.mkdir(exist_ok=True, parents=True)
 
     #%% Run it
     main(repo_old, repo_new, outpath)
