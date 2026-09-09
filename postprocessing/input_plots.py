442afbbf851fb6ba148b7a23c2a56275159f17c1
Patrick Brown <25125211+patrickbrown4@users.noreply.github.com>
2026-09-09 15:59:46 -0600

    add and use spatial.site2poly_buffer()

--- postprocessing/input_plots.py
+++ postprocessing/input_plots.py
@@ -1050,9 +1050,9 @@ def map_supplycurves(
         costadder = float(sw.GSw_TransIntraCost) * inflatable[2004, dollaryear]
     else:
         costadder = 0
-    ## Convert from point to polygons if desired (raster is 11.52 km but include a little extra)
+    ## Convert from points to polygons if desired
     if not markers:
-        dfsc.geometry = dfsc.buffer(11530/2, cap_style='square')
+        dfsc = reeds.spatial.site2poly_buffer(dfsc)
 
     ###### Format inputs
     ## Use 4.5 for limited access wind-ofs
